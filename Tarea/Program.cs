621017a38c2f8461973ae054877e2993a1c2bc0e
ERAU MSSA Student <ERAU MSSA Student>
2017-06-12 10:49:15 -0400

    Add project files.

---
+++ Tarea/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the Base of the triangle?");
+            string Tbase = Console.ReadLine();
+            double baseT = Convert.ToDouble(Tbase);
+
+            Console.WriteLine("What is the Height of the triangle?");
+            string Theight = Console.ReadLine();
+            double heightT = Convert.ToDouble(Theight);
+
+            double formulaT = .5;
+
+            Console.WriteLine(heightT * baseT * formulaT);
+            Console.ReadLine();
+
+
+        }
+    }
+}
